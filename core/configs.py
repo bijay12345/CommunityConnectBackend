fa4e587f16c949f4ca692adbc93f1bea7da1f4fc
bijay12345 <sbijay433@gmail.com>
2026-06-29 00:02:38 +0530

    feat: db connections

---
+++ core/configs.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings:
+    DB_HOST =  os.getenv("DB_HOST")
+    DB_PORT = os.getenv("DB_PORT")
+    DB_USER = os.getenv("DB_USER")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
+    DB_NAME =  os.getenv("DB_NAME")
+
+    @property
+    def get_db_url(self) -> str:
+        return f"mysql+pymysql://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+settings = Settings()
